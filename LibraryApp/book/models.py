867bc0d85be2cdb81c690037358e27b411c1d562
lourine <lourine.millicent@healthcloud.co.ke>
2022-03-08 07:56:37 +0300

    Update  Book Module model fields

--- LibraryApp/book/models.py
+++ LibraryApp/book/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from LibraryApp.authentication.models import User
 class Book(models.Model):
     name = models.CharField(max_length = 50)
-    picture = models.ImageField()
-    author = models.CharField(max_length = 30, default='anonymous')
+    author = models.CharField(max_length = 100, default='anonymous')
     email = models.EmailField(blank = True)
-    describe = models.TextField(default ='Crud on Library App')
+    describe = models.TextField(default ='Favorite Read')
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE,blank=True, null=True)
+
     def __str__(self):
         return self.name
